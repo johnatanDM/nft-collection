90c63ac781459245832e5b00a3e983d91a623150
Johnatan Chaves Ribeiro <johnatan.jcr@gmail.com>
2021-12-14 16:54:35 -0300

    deploy a smart contract that mint a NFT

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+PRIVATE_KEY = process.env.PRIVATE_KEY;
+ALCHEMY_URL = process.env.ALCHEMY_URL;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +21,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,22 +4,26 @@ const main = async () => {
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
 
+    // Call the function.
     let txn = await nftContract.makeAnEpicNFT()
-
+    // Wait for it to be mined.
     await txn.wait()
-
+    console.log("Minted NFT #1")
+  
     txn = await nftContract.makeAnEpicNFT()
-
+    // Wait for it to be mined.
     await txn.wait()
-    
+    console.log("Minted NFT #2")
   };
   
-( async () => {
+const runMain = async () => {
     try {
-      await main();
-      process.exit(0);
+        await main();
+        process.exit(0);
     } catch (error) {
-      console.log(error);
-      process.exit(1);
+        console.log(error);
+        process.exit(1);
     }
-  })();
+};
+  
+runMain();
